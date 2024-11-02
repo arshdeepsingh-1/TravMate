d6ec730096961254d85a30baa93aab2d8f8bcc92
arshdeepsingh-1 <asingh250804@gmail.com>
2024-11-02 14:06:18 -0400

    Main folder added

---
+++ TravMateApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravMateApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
